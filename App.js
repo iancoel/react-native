fd26077f763221209da58f4e622042806e80538b
Ian Coelho <ian.coel@hotmail.com>
2021-11-14 17:53:37 -0300

    text, view and stylesheet

--- App.js
+++ App.js
@@ -5,7 +5,16 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!!!</Text>
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello, world.</Text>
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.boldText}>
+          Lorem ipsum <Text>test</Text> dolor sit amet.
+        </Text>
+        <Text>Lorem ipsum dolor sit amet.</Text>
+        <Text>Lorem ipsum dolor sit amet.</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +26,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header: {
+    backgroundColor: 'pink',
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+  body: {
+    backgroundColor: 'yellow',
+    padding: 20,
   },
 });
